b72e597ada949e575f27843dd94b041412c7efeb
Touseef Ahmed <84684496+rainyjoke@users.noreply.github.com>
2024-03-10 03:32:49 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template
+from dictionary_dataset import dataset
+import random
+
+app = Flask(__name__)
+
+# Applying configuration of flask    ---To be kept secret---
+app.config['SECRET_KEY'] = 'Aaposi@234#*Joids9J89#&^Bvbiux/Biubc8*7'
+app.config['RECAPTCHA_ENABLED'] = True
+app.config['RECAPTCHA_PUBLIC_KEY'] = '5ids9J89#&^Bvbiux/BiubcvD3V5FfJGv4Z3YDp2YzK9wv'   
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6Ld5JZgUAAAAAExy5vD3V5FfJGv4Z3YDp2YzK9wv'
+app.config['RECAPTCHA_TABINDEX'] = 1
+app.config['RECAPTCHA_DATA_ATTRS'] = {'theme': 'dark', 'size': 'compact'}
+
+@app.route('/')
+def home():
+    
+    fe_items = get_data(1)
+    new_items = get_data(1)
+    return render_template("home.html", fe_items = fe_items, new_items = new_items)
+
+def get_data(num = 10):
+    dataset_men = get_random_data(filter_data(dataset['Men'], 'Innerwear'), num)
+    dataset_women = get_random_data(filter_data(dataset['Women'], 'Innerwear'), num)
+    dataset_boys = get_random_data(dataset['Boys'], num)
+    dataset_girls = get_random_data(dataset['Girls'], num)
+    dataset_unisex = get_random_data(dataset['Unisex'], num)
+    dataset_final = dataset_men + dataset_women + dataset_boys + dataset_girls + dataset_unisex
+    return dataset_final
+
+def filter_data(list, remove_filter):
+    return [d for d in list if d['subCategory'] != remove_filter]
+
+def get_random_data(list, num = 10):
+    return random.sample(list, num)
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/store')
+def store():
+    fe_items = get_data(4)
+    new_items = get_data(4)
+    return render_template("store.html", fe_items = fe_items, new_items = new_items)
+
+@app.route('/product/<gender>/<item_id>')
+def product(gender, item_id):
+    product_details = get_product_details(gender, item_id)
+    fe_items = get_random_data(filter_data(dataset[gender]  , 'Innerwear'), 4)
+    return render_template('product.html', product_details=product_details, fe_items=fe_items)
+
+
+def get_product_details(gender, item_id):
+    for d in dataset[gender]:
+        if d['id'] == item_id:
+            return d
+
+@app.route('/cart')
+def cart():
+    return render_template("cart.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ helper_functions.py
@@ -0,0 +1,151 @@
+import random
+import csv
+import json
+
+# function to get data's from CSV files and convert it into dictionary
+def getData():
+    images_file_name = 'C:/Users/Hp/Dropbox/PC/Desktop/Personal/uni_west_london/sem_4/Group Research Project 2024/fashion-dataset/images.csv'
+    styles_file_name = 'C:/Users/Hp/Dropbox/PC/Desktop/Personal/uni_west_london/sem_4/Group Research Project 2024/fashion-dataset/styles.csv'
+    data_men = {}
+    data_women = {}
+    data_boys = {}
+    data_girls = {}
+    data_unisex = {}
+    data_complete = {"Men": data_men,
+                     "Women": data_women,
+                     "Boys": data_boys,
+                     "Girls": data_girls,
+                     "Unisex": data_unisex}
+    img = {}
+
+    with open(styles_file_name, mode='r') as styles_file, open(images_file_name, mode='r') as images_file:
+        csv_reader = csv.reader(styles_file)
+        img_reader = csv.reader(images_file)
+       
+        for row in img_reader:
+            num = row[0].split(".")[0]
+            img[num] = row[1]
+
+        for row in csv_reader:
+           if row[0] != "id":
+                gender = row[1]  
+                metadata = row[2:] + [img[row[0]]]
+
+                if gender == "Men":
+                    data_men[row[0]]  = metadata
+
+                elif gender == "Women":
+                    data_women[row[0]] = metadata
+
+                elif gender == "Boys":
+                    data_boys[row[0]] = metadata
+
+                elif gender == "Girls":
+                    data_girls[row[0]] = metadata
+
+                else:
+                    data_unisex[row[0]] = metadata
+    return data_complete
+
+# function that takes in data and creates a new file with the data
+def writeDataToFile(data_complete, output_file_name):
+    with open(output_file_name, mode='w', newline='') as output_file:
+        csv_writer = csv.writer(output_file)
+
+        # Write header
+        csv_writer.writerow(['gender', 'id', 'masterCategory', 'subCategory', 'articleType', 'baseColour', 'season', 'year', 'usage', 'productDisplayName', 'link'])
+
+        # Write data from data_complete dictionary
+        for gender, data in data_complete.items():
+            for item_id, metadata in data.items():
+                row = [gender, item_id] + metadata
+                csv_writer.writerow(row)
+
+# function to read data from the file and convert it into dictionary
+def readDataFromFile(input_file_name):
+    data_complete = {}
+    
+    with open(input_file_name, mode='r') as input_file:
+        csv_reader = csv.reader(input_file)
+        next(csv_reader)  # Skip the header row
+        
+        for row in csv_reader:
+            gender = row[0]
+            item_id = row[1]
+            metadata = row[2:]
+            
+            if gender not in data_complete:
+                data_complete[gender] = {}
+            
+            data_complete[gender][item_id] = metadata
+    
+    return data_complete
+
+# Function to generate random price between 10 and 60 with two decimal places
+def generate_random_price():
+    return round(random.uniform(10, 60), 2)
+
+# Function that adds generated prices column in the dataset
+def add_column_price():
+    input_file_name = 'output_dataset.csv'
+    output_file_name = 'updated_dataset.csv'
+
+    # Read data from the input CSV file and add a new column with randomly generated prices
+    with open(input_file_name, mode='r') as input_file, open(output_file_name, mode='w', newline='') as output_file:
+        csv_reader = csv.reader(input_file)
+        csv_writer = csv.writer(output_file)
+
+        header = next(csv_reader)
+        header.append('Price')
+        csv_writer.writerow(header)
+
+        # Process each row and add randomly generated price
+        for row in csv_reader:
+            price = generate_random_price()
+            row.append(price)
+            csv_writer.writerow(row)
+
+# Function to convert data from CSV file to dictionary and write it to a text file
+
+def read_csv_and_create_dictionary(csv_filename):
+    # Initialize an empty dictionary to store the data
+    import csv
+    data_dict = {
+        "Men": [],
+        "Women": [],
+        "Boys": [],
+        "Girls": [],
+        "Unisex": []
+    }
+
+    # Read data from the CSV file and populate the dictionary
+    with open(csv_filename, mode='r', newline='', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            # Create a dictionary for the current row
+            item = {
+                'gender': row['gender'],
+                'id': row['id'],
+                'masterCategory': row['masterCategory'], 
+                'subCategory': row['subCategory'], 
+                'articleType': row['articleType'], 
+                'baseColour': row['baseColour'], 
+                'season': row['season'], 
+                'year': row['year'], 
+                'usage': row['usage'], 
+                'productDisplayName': row['productDisplayName'], 
+                'link': row['link'],
+                'price': row['price']
+            }
+
+            # Append the item to the appropriate list based on gender
+            data_dict[row['gender']].append(item)
+
+    return data_dict
+
+def write_data_to_textfile(data, output_file):
+    import json
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(json.dumps(data, indent=4))
+
+
