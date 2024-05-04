ff8b6d67448de41579898ec4f8fe6df8beef3a89
Touseef Ahmed <84684496+rainyjoke@users.noreply.github.com>
2024-05-04 14:11:11 +0400

    checkout page modified

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -11,6 +11,41 @@ document.getElementById("use-saved-address").addEventListener("change", function
 });
 
 document.getElementById("pay").addEventListener("click", function() {
-    
+    // Gather all the form data
+    var fullname = document.getElementById('fullname').value;
+    var phone = document.getElementById('phone').value;
+    var address = document.getElementById('address').value;
+    var city = document.getElementById('city').value;
+    var zipcode = document.getElementById('zipcode').value;
+
+    // Construct the JSON object with all the details
+    var formData = {
+        fullname: fullname,
+        phone: phone,
+        address: address,
+        city: city,
+        zipcode: zipcode
+    };
+
+    // Send the data to the /complete_order route in Flask
+    fetch("/complete_order", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }).then(function(response) {
+        return response.json();
+    }
+    ).then(function(data) {
+        if (data.status == "success") {
+            // Redirect the user to the order confirmation page
+            window.location.href = "/order_confirmation";
+        }
+    }
+    ).catch(function(error) {
+        console.log('Request failed', error);
+    });
+
 });
 
